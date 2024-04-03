201bb0f247441d5142ddc92e8a29dd7d1ad2673c
jonathan <jonathanpressler@icloud.com>
2024-04-03 19:20:37 -0400

    Update launcher.py to use subprocess for running API and Web servers

--- launcher.py
+++ launcher.py
@@ -1,10 +1,13 @@
 # Description: This is the main file that will be used to launch the application
-import asyncio
+import os
+import subprocess
 
 if __name__ == '__main__':
-    PA = ParrotAPI()
-    asyncio
-    PA.run()
-    PW = ParrotWeb()
-    PW.run()
-    print('API and Web servers started.')
+    api_command = 'nohup python api.py > api.log 2>&1 &'
+    web_command = 'nohup python -m web.web > web.log 2>&1 &'
+    subprocess.run(api_command, shell=True)
+    subprocess.run(web_command, shell=True)
+    api_pid = subprocess.check_output(['pgrep', '-f', 'api.py'], text=True).strip().split()[-1]
+    web_pid = subprocess.check_output(['pgrep', '-f', 'web.web'], text=True).strip().split()[-1]
+    print(f'API PID: {api_pid}')
+    print(f'Web PID: {web_pid}')
