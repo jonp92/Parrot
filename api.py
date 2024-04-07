09984dbb9be83ad0568a4a7d035850c9101ad2b5
jonathan <jonathanpressler@icloud.com>
2024-04-07 10:13:36 -0400

    Add additional origins to allowed origins list

--- api.py
+++ api.py
@@ -41,7 +41,7 @@ class ParrotAPI(Parrot):
         self.app = FastAPI()
         self.server_ip = self.get_server_ip()
         self.hostname = socket.gethostname()
-        self.origins = [self.server_ip, f'http://{self.server_ip}', f'http://{self.server_ip}:{self.web_port}', f'http://{self.hostname}', f'http://{self.hostname}:{self.web_port}']
+        self.origins = [self.server_ip, f'http://{self.server_ip}', f'http://{self.server_ip}:{self.web_port}', f'http://{self.hostname}', f'http://{self.hostname}:{self.web_port}', f'http://{self.hostname}.local', f'http://{self.hostname}.local:{self.web_port}']
         print(f'Allowed origins: {self.origins}')
         self.app.add_middleware(
             CORSMiddleware,
